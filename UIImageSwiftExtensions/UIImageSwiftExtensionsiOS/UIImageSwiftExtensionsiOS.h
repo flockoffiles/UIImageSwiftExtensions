6475ded7bd32b98078324326268c65c90732ad6d
Sergey A. Novitsky <sergey.novitsky@ing.nl>
2018-01-22 18:38:35 +0100

    Created and shared a framework scheme

---
+++ UIImageSwiftExtensions/UIImageSwiftExtensionsiOS/UIImageSwiftExtensionsiOS.h
@@ -0,0 +1,19 @@
+//
+//  UIImageSwiftExtensionsiOS.h
+//  UIImageSwiftExtensionsiOS
+//
+//  Created by Sergey Novitsky on 22/01/2018.
+//  Copyright © 2018 Giacomo Boccardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageSwiftExtensionsiOS.
+FOUNDATION_EXPORT double UIImageSwiftExtensionsiOSVersionNumber;
+
+//! Project version string for UIImageSwiftExtensionsiOS.
+FOUNDATION_EXPORT const unsigned char UIImageSwiftExtensionsiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageSwiftExtensionsiOS/PublicHeader.h>
+
+

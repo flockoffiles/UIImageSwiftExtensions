32470ae02d78f13f7fa9845cff9e19322c25dcfe
Sergey A. Novitsky <sergey.novitsky@ing.nl>
2018-01-23 12:58:29 +0100

    Renamed folders and the framework name

--- UIImageSwiftExtensions/UIImageSwiftExtensions/UIImageSwiftExtensions.h
+++ UIImageSwiftExtensions/UIImageSwiftExtensions/UIImageSwiftExtensions.h
@@ -9,11 +9,11 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for UIImageSwiftExtensionsiOS.
-FOUNDATION_EXPORT double UIImageSwiftExtensionsiOSVersionNumber;
+FOUNDATION_EXPORT double UIImageSwiftExtensionsVersionNumber;
 
 //! Project version string for UIImageSwiftExtensionsiOS.
-FOUNDATION_EXPORT const unsigned char UIImageSwiftExtensionsiOSVersionString[];
+FOUNDATION_EXPORT const unsigned char UIImageSwiftExtensionsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <UIImageSwiftExtensionsiOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageSwiftExtensions/PublicHeader.h>
 
 
